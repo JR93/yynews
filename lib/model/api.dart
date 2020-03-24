1660cd04d2327c770ff30e3b13fd2ade4f042651
JR93 <gjr_china@163.com>
2020-03-24 11:58:52 +0800

    Update api.dart

--- lib/model/api.dart
+++ lib/model/api.dart
@@ -3,7 +3,7 @@ import 'package:dio/dio.dart';
 Dio dio = new Dio();
 
 class Api {
-  static const String BaseUrl = "http://yue.yy.com/";
+  static const String BaseUrl = "http://xxx.xx.com/";
 
   static const String TOPIC = BaseUrl + "1.0/h5/message/getMessagesByTopic";
 
